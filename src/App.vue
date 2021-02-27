95979b579d08d296b1733c5e4e37f8afbc4af422
bilal <bilal.rizwaan1909@gmail.com>
2021-02-27 16:26:26 +0500

    Some More Changes

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
           <router-link to="/Sidebar">Sidebar</router-link>
           <router-link to="/Footer">Footer</router-link> -->
           <!-- <Navbar/> -->
-          <!-- <transition name="slide" mode="out-in"> -->
                   <router-view></router-view>
-          <!-- </transition> -->
           <!-- <Sidebar/> -->
           <!-- <Content/> -->
           <!-- <Footer/> -->
@@ -42,12 +40,12 @@ export default {
 <style  scoped>
 @import "./assets/Css/style.css"; 
 /* @import "./assets/admin-lte/css/adminlte.scss"; */
-/* .slide-enter-active , .slide-enter-active{
+.slide-enter-active , .slide-enter-active{
   transition: opacity 1 , transform 1;
 }
 .slide-leave , .slide-leave-to{
   opacity: 0;
   transform: translateX(-30%);
 
-} */
+}
 </style>
